a9ebcfc3154835dbd4f9c5581727c92337567037
Griffone <thegriffones@gmail.com>
2022-04-19 01:24:18 +0200

    Update bootstrap script

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,11 +1,15 @@
 -- Get all the relevant files from the repository
 
 local args = {...}
-local outDir = (args[0] or "gnet").."/"
-
-shell.run("wget", "https://raw.githubusercontent.com/rxi/json.lua/master/json.lua", "json")
+local outDir = (args[0] or "glib").."/"
 
 local json = require("json")
+
+if json == nil then
+	shell.run("wget", "https://raw.githubusercontent.com/rxi/json.lua/master/json.lua")
+end
+
+json = require("json")
 
 local response = http.get("https://api.github.com/repos/technomunk/cc-glib/contents/src")
 assert(response.getResponseCode() == 200, "failed to get repository contents")
@@ -13,7 +17,5 @@ assert(response.getResponseCode() == 200, "failed to get repository contents")
 local files = json.decode(response.readAll())
 for _, file in ipairs(files) do
 	assert(file["type"], "unsupported file type: "..file["type"])
-	local name = file["name"]
-	name = name:sub(1, (name:find("%.lua") or 0) - 1)
-	shell.run("wget", file["download_url"], outDir..name)
+	shell.run("wget", file["download_url"], outDir..file["name"])
 end
