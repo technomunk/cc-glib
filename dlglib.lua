ade8e5e001bd97e90702913785b0d3ac6dbfcd9f
technomunk <thegriffones@gmail.com>
2025-11-30 20:47:50 +0100

    Don't rely on # operator

--- dlglib.lua
+++ dlglib.lua
@@ -19,7 +19,7 @@ if dir:sub(#dir) ~= "/" then
 end
 
 local requests = {}
-local expectedFiles = 0
+local pendingRequests = 0
 
 local function requestAllFilesInDir(resp)
 	local files = textutils.unserializeJSON(resp.readAll())
@@ -27,13 +27,14 @@ local function requestAllFilesInDir(resp)
 		if file["type"] == "file" then
 			local filename = string.sub(file["path"], 5)
 			requests[file["download_url"]] = dir..filename
-			expectedFiles = expectedFiles + 1
+			pendingRequests = pendingRequests + 1
 			http.request(file["download_url"])
 		elseif file["type"] == "dir" then
 			local filename = string.sub(file["path"], 5)
 			fs.makeDir(dir..filename)
 			url = "https://api.github.com/repos/technomunk/cc-glib/contents/"..file["path"]
 			requests[url] = true
+			pendingRequests = pendingRequests + 1
 			http.request(url)
 		else
 			printError("Unknown file type:", file[type])
@@ -46,23 +47,24 @@ assert(resp.getResponseCode() == 200, "failed to get repository contents")
 
 requestAllFilesInDir(resp)
 
-local downloaded, failed = 0, 0
+local downloaded = 0
 
-while (downloaded + failed) < (#requests) do
+while pendingRequests > 0 do
 	local event, url, resp = os.pullEvent()
 
 	if event == "http_success" then
 		if type(requests[url]) == "boolean" then
 			requestAllFilesInDir(resp)
 			downloaded = downloaded + 1
+			pendingRequests = pendingRequests - 1
 		elseif requests[url] then
 			saveFile(requests[url], resp.readAll())
 			downloaded = downloaded + 1
+			pendingRequests = pendingRequests - 1
 		end
 	elseif event == "http_failure" then
 		print("couldn't download "..requests[url]..": "..resp)
-		failed = failed + 1
 	end
 end
 
-print("downloaded "..downloaded.."/"..expectedFiles.." files")
+print("downloaded", downloaded, "files")
